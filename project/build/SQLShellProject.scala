5b3cc46861748f62344e9d6a87d13f2639fee43c
Brian Clapper <bmc@allegro.inside.clapper.org>
2009-07-19 00:12:00 -0400

    Added external dependencies for jopt-simple and joda time.

--- project/build/SQLShellProject.scala
+++ project/build/SQLShellProject.scala
@@ -10,6 +10,9 @@ class SQLShellProject(info: ProjectInfo) extends DefaultProject(info)
         "http://scala-tools.org/repo-releases/org/scala-tools/testing/scalatest/0.9.5/"
 
     val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5"
+    val joptSimple = "net.sf.jopt-simple" % "jopt-simple" % "3.1"
+    val jodaTime = "joda-time" % "joda-time" % "1.6"
+
     // Grizzled comes from local machine for now
     val grizzled = "grizzled-scala-library" % "grizzled-scala-library" % "0.1" from 
         "http://darkroom.inside.clapper.org/~bmc/code/grizzled-scala-library-0.1.jar"
