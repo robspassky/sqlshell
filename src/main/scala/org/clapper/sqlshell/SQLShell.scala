35273f3b83eda52b8ee4b8c308daee4d05b011c7
Brian Clapper <bmc@clapper.org>
2009-07-28 20:25:37 -0400

    Adjusted to accounts for changes in packages for GrizzledString

--- src/main/scala/org/clapper/sqlshell/SQLShell.scala
+++ src/main/scala/org/clapper/sqlshell/SQLShell.scala
@@ -5,7 +5,7 @@ import grizzled.readline.{ListCompleter, PathnameCompleter}
 import grizzled.readline.Readline
 import grizzled.readline.Readline.ReadlineType
 import grizzled.readline.Readline.ReadlineType._
-import grizzled.GrizzledString._
+import grizzled.string.implicits._
 import grizzled.string.WordWrapper
 import grizzled.config.Configuration
 import grizzled.math.util._
