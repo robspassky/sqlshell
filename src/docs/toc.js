b5af1b50f9a884abdd2b6e85e22828a89e583aaa
Brian M. Clapper <bmc@clapper.org>
2010-03-23 09:21:59 -0400

    Minor change to stylesheet for generated docs

--- src/docs/toc.js
+++ src/docs/toc.js
@@ -6,49 +6,45 @@
 function createTOC()
 {
     // find the nodes to be added to the Page TOC
-
-  var tocTargets = new Array()
-  nodes = document.getElementById('body').childNodes
-  for (var i = 0; i < nodes.length; i++)
-  {
-    nn = nodes[i].nodeName
-    if (nn == "H1" || nn == "H2" || nn == "H3" ||
-        nn == "H4" || nn == "H5" || nn == "H6")
+    var tocTargets = new Array();
+    var nodes = document.getElementById('body').childNodes;
+    for (var i = 0; i < nodes.length; i++)
     {
-      tocTargets.push(nodes[i])
-    }
-  }
-
-  tocDiv = document.getElementById('toc')
-
-  // Remove toc if none or one heading
-  if (tocTargets.length <= 1)
-  {
-    tocDiv.parentNode.removeChild(tocDiv)
-    return;
-  }
-
-  // Add the toc contents
-  tocDiv = document.getElementById('toc')
-  //tocDiv.innerHTML= "<h2>Page Contents </h2>"
-  tocList = document.createElement('ul')
-  tocList.className = 'toc'
-  tocDiv.appendChild(tocList)
-  // Insert elements into our table of contents
-  for (var i = 0; i < tocTargets.length; i++)
-  {
-    tocTarget = tocTargets[i]
-    if (tocTarget.id == '')
-    {
-      tocTarget.id = 'toc' + i
+        var nn = nodes[i].nodeName
+        if (nn == "H1" || nn == "H2" || nn == "H3" ||
+            nn == "H4" || nn == "H5" || nn == "H6")
+        {
+            tocTargets.push(nodes[i]);
+        }
     }
 
-    newItem = document.createElement('li')
-    newItem.className = 'toc' + tocTarget.nodeName
-    newLink = document.createElement('a')
-    newLink.href = '#' + tocTarget.id
-    newLink.innerHTML = tocTarget.innerHTML
-    newItem.appendChild(newLink)
-    tocList.appendChild(newItem)
-  }  
+    var tocDiv = document.getElementById('toc');
+
+    // Remove toc if none or one heading
+    if (tocTargets.length <= 1)
+    {
+        tocDiv.parentNode.removeChild(tocDiv);
+        return;
+    }
+
+    // Add the toc contents
+    tocDiv = document.getElementById('toc');
+    tocList = document.createElement('ul');
+    tocList.className = 'toc';
+    tocDiv.appendChild(tocList);
+    // Insert elements into our table of contents
+    for (var i = 0; i < tocTargets.length; i++)
+    {
+        tocTarget = tocTargets[i];
+        if (tocTarget.id == '')
+            tocTarget.id = 'toc' + i;
+
+        var newItem = document.createElement('li');
+        newItem.className = 'toc' + tocTarget.nodeName;
+        var newLink = document.createElement('a');
+        newLink.href = '#' + tocTarget.id;
+        newLink.innerHTML = tocTarget.innerHTML;
+        newItem.appendChild(newLink);
+        tocList.appendChild(newItem);
+    }
 }
